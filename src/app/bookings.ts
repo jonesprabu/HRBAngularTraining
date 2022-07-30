a7fe4b38e93b226f12a07a1c1b11dff4dfd57dde
RichardAblett <richard.ablett@hrblock.com>
2022-07-30 16:08:50 +0530

    Booking

---
+++ src/app/bookings.ts
@@ -0,0 +1,15 @@
+export class Bookings {
+    name:string='';
+    phone:number=0;
+    email:string='';
+    taxi:string='';
+    extra:string='';
+    dateTime:string='';
+    pikupPlace:string='';
+    dropoffPlace:string='';
+    instructions:string='';
+    id:number=0;
+}
+
+
+
