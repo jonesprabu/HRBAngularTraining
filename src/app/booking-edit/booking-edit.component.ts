d032bf77d475346ff1ef32a1ffad72c1d8d6367d
Shobith <shobith.chandran@hrblock.com>
2022-07-30 18:22:36 +0530

    Changes

--- src/app/booking-edit/booking-edit.component.ts
+++ src/app/booking-edit/booking-edit.component.ts
@@ -9,6 +9,10 @@ import { FormClass } from 'app/form-class';
   styleUrls: ['./booking-edit.component.scss']
 })
 export class BookingEditComponent implements OnInit {
+
+  BookList: FormClass[] = [];
+  cus: FormClass = new FormClass();
+
   booking: FormClass ={
     customer_name: '',
     id: 0,
@@ -21,6 +25,7 @@ export class BookingEditComponent implements OnInit {
     dropoff_place: '',
     comments: ''
   };
+  
 
   constructor(private route: ActivatedRoute, private bookingService: BookingService) { }
 
@@ -30,9 +35,21 @@ export class BookingEditComponent implements OnInit {
 
     this.bookingService.getBooking(id).subscribe((data: BookingService)=>{
       console.log("display employee list..."+data);
-      this.booking = data;
-      console.log(this.booking);
+      this.bookingService = data;
+      console.log(this.bookingService);
     });
+  }
+
+  //to update a new employee from the db.json
+  updateEmployeeDetails(): void {
+    console.log(this.booking);
+    this.bookingService.updateBooking(this.booking).subscribe((data)=>{
+      console.log("Employee Updated Successfully...");
+    });
+  }
+
+  onExtrasChange(event: any) {
+    this.cus.extras = this.cus.extras + " " + event.target.value
   }
 
 }
